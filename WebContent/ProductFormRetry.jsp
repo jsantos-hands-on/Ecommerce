0ed6f2c486f0780cb55ef91e0c4c069dcbf00e71
jsantos - out home <joaomarcosfelixds@gmail.com>
2019-02-04 20:04:02 -0300

    Setting ProductBean and ProductDAO(insert)

---
+++ WebContent/ProductFormRetry.jsp
@@ -0,0 +1,29 @@
+<%@ 
+page language="java" 
+contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"
+import="br.com.model.ProductBean"
+%>
+
+<!DOCTYPE html>
+
+<jsp:useBean id="product" class="br.com.model.ProductBean" />
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Retry...</title>
+	</head>
+	<body>
+		<h1>Product registration!</h1>
+		<form action="ProductProcessor.jsp" method="post">
+			Name: <input type="text" name="name" value="${product.name}"/>
+			<%=product.getMistakes("name")%>
+			<br />
+			Price: <input type="text" name="price" value="${product.price}"/>
+			<%=product.getMistakes("price")%>
+			<br />
+			<input type="submit" value="Save"/>
+			<input type="reset" value="Clean">
+		</form>
+	</body>
+</html>
--- src/br/com/web/RegisterProduct.java
+++ src/br/com/web/RegisterProduct.java
@@ -8,18 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import br.com.dao.ConnectionPool;
 import br.com.dao.ProductDAO;
-import br.com.model.Product;
+import br.com.model.ProductBean;
 
 public class RegisterProduct implements Task {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
 
-		String name = req.getParameter("name");
-		Double price = Double.parseDouble(req.getParameter("price"));
-		Product product = new Product();
-		product.setName(name);
-		product.setPrice(price);
+		//String name = req.getParameter("name");
+		//Double price = Double.parseDouble(req.getParameter("price"));
+		ProductBean product = (ProductBean) req.getAttribute("product");
+		//product.setName(name);
+		//product.setPrice(price);
 
 		boolean flag = false;
 
